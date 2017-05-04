aefd9873914fa037625cc9b01a66944568441848
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2017-05-04 09:44:09 -0300

    flip flop lib folder with a d type flip flop

---
+++ lib/flip_flops/d_type_flip_flop.vhd
@@ -0,0 +1,18 @@
+ENTITY d_type_flip_flop IS
+
+	PORT(clock, input: IN STD_LOGIC; output, inv_output: INOUT STD_LOGIC);
+	
+END d_type_flip_flop;
+
+
+ARCHITECTURE data_flow OF d_type_flip_flop IS
+
+	SIGNAL inv_in: STD_LOGIC;
+	
+BEGIN
+
+	inv_in <= NOT input
+	output <= (input NAND clock) NOR inv_output;
+	inv_output <= (inv_in NAND clock) NOR output; 
+	
+END data_flow;
