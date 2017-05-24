c0a7eeb3feffc7a83e9431c65c67cf20fd0f1f48
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2017-05-24 08:26:58 -0300

    changed the carry implementation because of a bug with the output

--- single_digit_decimal_counter.vhd
+++ single_digit_decimal_counter.vhd
@@ -7,12 +7,16 @@ ENTITY single_digit_decimal_counter IS
 		enable_in, true_reset, clock: IN STD_LOGIC;
 		limit: IN STD_LOGIC_VECTOR(3 DOWNTO 0); 
 		output: INOUT STD_LOGIC_VECTOR(3 DOWNTO 0);
-		carry: INOUT STD_LOGIC
+		carry: OUT STD_LOGIC
 	);
 	
 END single_digit_decimal_counter;
 
 ARCHITECTURE structural OF single_digit_decimal_counter IS
+	
+	COMPONENT and2
+		PORT(in_a, in_b: IN STD_LOGIC; output: OUT STD_LOGIC);
+	END COMPONENT;
 	
 	COMPONENT counterlimiter
 		PORT(
@@ -27,10 +31,6 @@ ARCHITECTURE structural OF single_digit_decimal_counter IS
 		PORT(clock, input: IN STD_LOGIC; output, inv_output: INOUT STD_LOGIC);
 	END COMPONENT;
 	
-	COMPONENT prop
-		PORT(input, carry_in: IN STD_LOGIC; output, carry_out: OUT STD_LOGIC );
-	END COMPONENT;
-	
 	COMPONENT normalizer
 		PORT(
 			input: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -39,13 +39,24 @@ ARCHITECTURE structural OF single_digit_decimal_counter IS
 		);
 	END COMPONENT;
 	
+	COMPONENT not1
+		PORT(input: IN STD_LOGIC; output: OUT STD_LOGIC);
+	END COMPONENT;
+
+	COMPONENT prop
+		PORT(input, carry_in: IN STD_LOGIC; output, carry_out: OUT STD_LOGIC );
+	END COMPONENT;
+
+	FOR ALL: and2 USE ENTITY work.and_2 (data_flow);
 	FOR ALL: counterlimiter USE ENTITY work.counter_limiter(structural);
 	FOR ALL: dflipflop USE ENTITY work.d_type_flip_flop(behaviour);
-	FOR ALL: prop USE ENTITY work.propagator(data_flow);
 	FOR ALL: normalizer USE ENTITY work.four_bit_vector_disabler(structural);
+	FOR ALL: not1 USE ENTITY work.not_1 (data_flow);
+	FOR ALL: prop USE ENTITY work.propagator(data_flow);
 	
 	SIGNAL e_mid: STD_LOGIC_VECTOR(2 DOWNTO 0);
 	SIGNAL p_out, norm_out, lm_out: STD_LOGIC_VECTOR(3 DOWNTO 0);
+	SIGNAL c_nrst, md_c: STD_LOGIC;
 BEGIN
 
 	-- enablers propagate
@@ -60,7 +71,7 @@ BEGIN
 	);
 	
 	-- reset over limit
-	lm: counterlimiter PORT MAP (enable_in, limit, norm_out, lm_out, carry);
+	lm: counterlimiter PORT MAP (enable_in, limit, norm_out, lm_out, md_c);
 	
 	-- set flip-flops get next output
 	d0: dflipflop PORT MAP (
@@ -91,5 +102,7 @@ BEGIN
 	
 	-- true reset
 	tr: normalizer PORT MAP(p_out, true_reset, output);
+	nrst: not1 PORT MAP(true_reset, c_nrst);
+	trc: and2 PORT MAP(c_nrst, md_c, carry);
 	
 END structural;
